aeeb5237d785c63d03341661d5b42862e41441f2
opensourceoeesoftware <info@opensourceoeesoftware.com>
2025-11-23 22:54:17 +0100

    latest build

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,7 +1,7 @@
 
- export const url ='http://' + window.location.hostname
+//  export const url ='http://' + window.location.hostname
 
- export const wsurl ='ws://' + window.location.hostname
+//  export const wsurl ='ws://' + window.location.hostname
 
-// export const url =''
-// export const wsurl = 'wss://' + window.location.hostname
+export const url =''
+export const wsurl = 'wss://' + window.location.hostname
